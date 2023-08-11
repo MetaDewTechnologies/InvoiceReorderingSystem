b4c882477f8561222b9cb7a1d01960adfaf80b77
AkilaYapa <akilayapa1998@gmail.com>
2023-08-12 01:13:44 +0530

    Initial commit

---
+++ API/InvoiceReorderingSystemAPI/src/test/java/com/metadew/metadewtechnologies/MetadewtechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metadew.metadewtechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadewtechnologiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
