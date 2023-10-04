84124134d213c4948787e97c2a3edb0f1ee532ae
AkilaYapa <akilayapa1998@gmail.com>
2023-10-04 18:54:52 +0530

    change httpClient file

--- Web/invoice-reordering-system/src/helpers/HttpClient.js
+++ Web/invoice-reordering-system/src/helpers/HttpClient.js
@@ -1,37 +1,6 @@
 import axios from "axios";
 
-var serviceUrl =
-  "http://Env-new-env.eba-ivwrktva.us-east-1.elasticbeanstalk.com";
-
-// export const CommonGet = (url, queryString) => {
-//   return new Promise((resolve, reject) => {
-//     let request = new XMLHttpRequest();
-//     if (queryString != null) {
-//       request.open("GET", serviceUrl + url + "?" + AESEncryption(queryString));
-//     } else {
-//       request.open("GET", serviceUrl + url);
-//     }
-
-//     request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-//     request.setRequestHeader("Access-Control-Allow-Methods", "*");
-//     request.setRequestHeader("Accept-Language", "en-US");
-//     request.onload = () => {
-//       if (request.response === "Token Time Exceed") {
-//         window.logout.logout();
-//       } else {
-//         if (request.status >= 200 && request.status < 300) {
-//           resolve(JSON.parse(AESDecryption(request.response)));
-//         } else {
-//           reject(request.statusText);
-//         }
-//       }
-//     };
-//     request.onerror = () => {
-//       reject(request.statusText);
-//     };
-//     request.send();
-//   });
-// };
+var serviceUrl = "http://localhost:8080";
 
 export const CommonGetAxios = async (url, queryString) => {
   const bearerToken = sessionStorage.getItem("token");
@@ -60,68 +29,6 @@ export const CommonGetAxios = async (url, queryString) => {
     }
   });
 };
-
-// export const CommonPost = (url, queryString, body) => {
-//   const bearerToken = sessionStorage.getItem("token");
-//   return new Promise((resolve, reject) => {
-//     let request = new XMLHttpRequest();
-//     if (queryString != null) {
-//       request.open("POST", serviceUrl + url + "?" + AESEncryption(queryString));
-//     } else {
-//       request.open("POST", serviceUrl + url);
-//     }
-//     request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-//     request.setRequestHeader("Access-Control-Allow-Methods", "*");
-//     request.setRequestHeader("Accept-Language", "en-US");
-//     request.setRequestHeader("Authorization", `Bearer ${bearerToken}`);
-//     request.setRequestHeader("Access-Control-Allow-Origin", "*");
-//     request.onload = () => {
-//       if (request.response === "Token Time Exceed") {
-//         window.logout.logout();
-//       } else {
-//         if (request.status >= 200 && request.status < 300) {
-//           resolve(JSON.parse(request.response));
-//         } else {
-//           reject(request.statusText);
-//         }
-//       }
-//     };
-//     request.onerror = () => {
-//       reject(request.statusText);
-//     };
-//     request.send(body);
-//   });
-// };
-
-// export const CommonPost = (url, queryString, body) => {
-//     const bearerToken = sessionStorage.getItem('token');
-//     let data = JSON.stringify(body);
-//     console.log("body", body);
-//     let originURL;
-//     if (queryString != null) {
-//         originURL = serviceUrl + url + "?" + AESEncryption(queryString);
-//     } else {
-//         originURL = serviceUrl + url;
-//     }
-//     let config = {
-//     method: 'post',
-//     maxBodyLength: Infinity,
-//     url: originURL,
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${bearerToken}`
-//     },
-//         data : data
-//     };
-//     console.log("config ",config)
-//     axios.request(config)
-//     .then((response) => {
-//     console.log(JSON.stringify(response.data));
-//     })
-//     .catch((error) => {
-//     console.log(error);
-//     });
-//     }
 
 export const CommonPostAxios = async (url, queryString, body) => {
   let originURL;
