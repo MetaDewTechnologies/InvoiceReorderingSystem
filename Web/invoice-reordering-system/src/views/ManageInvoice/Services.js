7f7c5c861336dc6dd938e5ae168e5f92aaaac932
AkilaYapa <akilayapa1998@gmail.com>
2024-01-06 21:51:31 +0530

    Search bill by date range front end implementation

--- Web/invoice-reordering-system/src/views/ManageInvoice/Services.js
+++ Web/invoice-reordering-system/src/views/ManageInvoice/Services.js
@@ -10,7 +10,8 @@ export default {
   deleteInvoiceItem,
   handleCreateInvoice,
   saveGreenTax,
-  getGreenTaxByInvoiceId
+  getGreenTaxByInvoiceId,
+  getBillsByDateRange,
 };
 
 async function saveInvoice(data) {
@@ -79,7 +80,15 @@ async function saveGreenTax(invoiceId, model) {
   const response = await CommonPostAxios("/api/v1/greenTax", invoiceId, model);
   return response;
 }
-async function getGreenTaxByInvoiceId(invoiceId){
-  const response = await CommonPostAxios('/api/v1/checkGreenTax',invoiceId,null)
+async function getGreenTaxByInvoiceId(invoiceId) {
+  const response = await CommonPostAxios(
+    "/api/v1/checkGreenTax",
+    invoiceId,
+    null
+  );
+  return response;
+}
+async function getBillsByDateRange(model) {
+  const response = await CommonPostAxios("/api/v1/all-invoices", null, model);
   return response;
 }
